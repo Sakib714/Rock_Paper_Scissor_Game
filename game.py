3f59a545b9380bc2e5360857bbb5b14ae4dafae8
Sakib714 <sakib39.official@gmail.com>
2022-02-26 23:20:00 +0600

    Added point system and decraling result function

--- game.py
+++ game.py
@@ -1,10 +1,6 @@
 from random import choice
 import pyttsx3
 
-
-
-
-
 engine = pyttsx3.init()
 
 # rate
@@ -20,7 +16,6 @@ def speak(text):
     engine.runAndWait()
 
 
-
 # welcome msg
 
 welcome_word = 'Welcome to rock papper scissor game. Enter r for rock, enter p for paper and enter s for scissor'
@@ -28,10 +23,8 @@ welcome_word = 'Welcome to rock papper scissor game. Enter r for rock, enter p f
 print(welcome_word)
 speak(welcome_word)
 
-
 speak('How many times do u want to play')
-times = int(input('How many times do u want to play: '))
-
+times = int(input('How many times do u want to play: ').lower())
 
 
 def is_win(user, computer):
@@ -39,34 +32,48 @@ def is_win(user, computer):
 
     if (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') or (user == 's' and computer == 'p'):
         return True
+
+
 # game start
-
-
-
-
 
 
 def play():
     speak('Enter a guess: ')
     user = input('Enter a guess: ')
 
-
     computer = choice(['r', 'p', 's'])
 
     if user == computer:
         return 'it\'s a tie'
 
-
     if is_win(user, computer):
-
-
-        return 'You Won!'
+        return 'You_Won!'
 
     return 'You Lose'
 
 
 # Gameplay
 
-for i in range(times):
-    print(play())
+point = 0
 
+for i in range(times):
+
+    gg = play()
+
+    print(gg)
+
+    if gg == 'You_Won!':
+        point += 1
+
+print(f'Total Point: ', point)
+
+# Calculate percentage
+
+percent = (point/times)*100
+
+print(f'You Scored total {percent}%')
+
+# Speak result
+
+speak(f'Your total point is {point}')
+speak(f'You scored total {percent}percent')
